67cc8985da257298d9fcb373ca6cf329366909e4
Nikola <nmarovic998@gmail.com>
2020-06-02 14:01:15 +0200

    aa

--- mojiJsFajlovi/osnovno_podesavanje.js
+++ mojiJsFajlovi/osnovno_podesavanje.js
@@ -4,3 +4,18 @@
         $("#preloder").delay(200).fadeOut("slow");
     });
 })(jQuery);
+
+//za galeriju na sredini ispod hedera
+
+$(".hero-items").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    items: 1,
+    dots: true,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    navText: ['<i class="arrow_carrot-left"></i>', '<i class="arrow_carrot-right"></i>'],
+    smartSpeed: 1200,
+    autoHeight: false,
+});
